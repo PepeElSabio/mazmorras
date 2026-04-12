ce6171f087cc31a72c6c057b99bde9a33b7a0ecf
PepeElSabio <pablo2004cv@gmail.com>
2026-04-12 15:41:33 +0200

    bloque de terreno

---
+++ scripts/terreno/bloque.gd
@@ -0,0 +1,16 @@
+class_name bloque
+extends  Node3D
+
+const SUELO : BoxMesh = preload("res://Assets/Terreno/suelo.tres")
+const PARED : BoxMesh = preload("res://Assets/Terreno/pared.tres")
+
+
+static func add(mesh: BoxMesh, tile_pos: Vector3i, tile_size: Vector3i, contenedor: Node3D) -> void:
+	var mesh_instance = MeshInstance3D.new()
+	var box = mesh.duplicate() as BoxMesh
+	box.size = Vector3(tile_size)
+	mesh_instance.mesh = box
+	mesh_instance.position = Vector3(tile_pos) + Vector3(tile_size) / 2.0 - Vector3.ONE
+	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_ON
+	mesh_instance.gi_mode = GeometryInstance3D.GI_MODE_STATIC
+	contenedor.add_child(mesh_instance)
